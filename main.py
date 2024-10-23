6a15caeff4f1bc1945b7729d3a00f64ac1f6bd7c
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2024-10-23 10:15:06 +0200

    complete engine load plot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 from datetime import datetime
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot(x: list, y: list, title: str, label: str, clear: bool = True):
@@ -53,26 +54,32 @@ def is_engine2(file_path: str) -> bool:
     return "Engine2" in str(file_path)
 
 
-def main():
+def generate_engine_load_plots():
     file_paths = Path("gunnerus").glob("**/*.csv")
     filtered_file_paths = [file_path for file_path in file_paths if is_engine(file_path) and is_engine_load(file_path)]
 
     all_data = {}
 
     for file_path in filtered_file_paths:
-        if is_engine2(file_path):
-            continue
         label = extract_label(file_path)
         time, data, unit = extract_data(file_path)
+        all_data[label] = (time, data)
         plot(time, data, "Individual Engine Loads", label, False)
 
-        for t, d in zip(time, data):
-            all_data[t] = all_data.get(t, 0) + d
+    all_times = sorted(set(t for time, _ in all_data.values() for t in time))
+    all_timestamps = np.array([t.timestamp() for t in all_times])
+    total_sum = np.zeros(len(all_timestamps))
 
-    all_time = sorted(all_data.keys())
-    all_sum = [all_data[t] for t in all_time]
+    for time, data in all_data.values():
+        time_timestamps = np.array([t.timestamp() for t in time])
+        interpolated_data = np.interp(all_timestamps, time_timestamps, data)
+        total_sum += interpolated_data
 
-    plot(all_time, all_sum, "Total Engine Load", "Total Load", False)
+    plot(all_times, total_sum, "Total Engine Load", "Total Load", False)
+
+
+def main():
+    generate_engine_load_plots()
 
 
 if __name__ == "__main__":
