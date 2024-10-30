d83eb45ce477cd0ac210f399d5b3cf8c6b77a8dd
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2024-10-30 09:18:45 +0100

    refactor: reorder function order

--- main.py
+++ main.py
@@ -4,6 +4,65 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 
+"""
+filters
+"""
+
+
+def is_engine2(file_path: str) -> bool:
+    return "Engine2" in str(file_path)
+
+
+# exclude plotting engine 2 which was not being used
+def is_engine(file_path: str) -> bool:
+    return "Engine" in str(file_path) and not is_engine2(file_path)
+
+
+def is_thruster(file_path: str) -> bool:
+    return "hcx" in str(file_path)
+
+
+def is_engine_load(file_path):
+    return "engine_load" in str(file_path) and is_engine(file_path)
+
+
+def is_engine_speed(file_path):
+    return "engine_speed" in str(file_path) and is_engine(file_path)
+
+
+def is_thruster_load_feedback(file_path):
+    return "LoadFeedback" in str(file_path) and is_thruster(file_path)
+
+
+def is_engine_exhaust_temperature1(file_path):
+    return "exhaust_temperature1" in str(file_path) and is_engine(file_path)
+
+
+def is_engine_exhaust_temperature2(file_path):
+    return "exhaust_temperature2" in str(file_path) and is_engine(file_path)
+
+
+def is_exhaust_temperature2(file_path):
+    return "exhaust_temperature2" in str(file_path) and is_engine(file_path)
+
+
+def is_engine_coolant_temperature(file_path):
+    return "coolant_temperature" in str(file_path) and is_engine(file_path)
+
+
+def is_engine_boost_pressure(file_path):
+    return "boost_pressure" in str(file_path) and is_engine(file_path)
+
+
+def is_engine_fuel_consumption(file_path):
+    return "fuel_consumption" in str(file_path) and is_engine(file_path)
+
+
+"""
+plots
+"""
+
+
 def plot(x: list, y: list, title: str, label: str, clear: bool = True, save: bool = True):
     plt.plot(x, y, label=label)
     plt.xticks(rotation=45)
@@ -15,79 +74,6 @@ def plot(x: list, y: list, title: str, label: str, clear: bool = True, save: boo
         plt.savefig("plots/" + title + ".svg")
     if clear:
         plt.clf()
-
-
-def extract_data(file_path: str):
-    label = " ".join(file_path.parts[-2:])
-    unit = ""
-    time = []
-    value = []
-    with open(file_path) as file:
-        for line in file.readlines():
-            elements = line.split(",")
-            if len(unit) == 0:
-                unit = elements[2]
-            time.append(datetime.fromisoformat(elements[0]))
-            value.append(round(float(elements[1]), 2))
-    return time, value, unit, label
-
-
-def is_engine(file_path: str) -> bool:
-    return "Engine" in str(file_path)
-
-
-def is_thruster(file_path: str) -> bool:
-    return "hcx" in str(file_path)
-
-
-def is_engine_load(file_path):
-    return "engine_load" in str(file_path)
-
-
-def is_engine_speed(file_path):
-    return "engine_speed" in str(file_path)
-
-
-def is_thruster_load_feedback(file_path):
-    return "LoadFeedback" in str(file_path)
-
-
-def is_engine_exhaust_temperature1(file_path):
-    return "exhaust_temperature1" in str(file_path)
-
-
-def is_exhaust_temperature2(file_path):
-    return "exhaust_temperature2" in str(file_path)
-
-
-def is_engine_coolant_temperature(file_path):
-    return "coolant_temperature" in str(file_path)
-
-
-def is_engine_boost_pressure(file_path):
-    return "boost_pressure" in str(file_path)
-
-
-def is_engine_fuel_consumption(file_path):
-    return "fuel_consumption" in str(file_path)
-
-
-def is_engine2(file_path: str) -> bool:
-    return "Engine2" in str(file_path)
-
-
-def specific_fuel_consumption(fuel_mass_flow, power):
-    return fuel_mass_flow/power
-
-
-def extract_time_series(filtered_file_paths):
-    all_data = {}
-
-    unit = ""
-    for file_path in filtered_file_paths:
-        time, data, unit, label = extract_data(file_path)
-        all_data[label] = (time, data)
-    return all_data, unit
 
 
 def plot_inidividual_and_sum(filtered_file_paths, plot_title):
@@ -116,6 +102,45 @@ def plot_individual(filtered_file_paths, plot_title):
     plt.clf()
 
 
+"""
+data processing
+"""
+
+
+def extract_data(file_path: str):
+    label = " ".join(file_path.parts[-2:])
+    unit = ""
+    time = []
+    value = []
+    with open(file_path) as file:
+        for line in file.readlines():
+            elements = line.split(",")
+            if len(unit) == 0:
+                unit = elements[2]
+            time.append(datetime.fromisoformat(elements[0]))
+            value.append(round(float(elements[1]), 2))
+    return time, value, unit, label
+
+
+def extract_time_series(filtered_file_paths):
+    all_data = {}
+
+    unit = ""
+    for file_path in filtered_file_paths:
+        time, data, unit, label = extract_data(file_path)
+        all_data[label] = (time, data)
+    return all_data, unit
+
+
+"""
+calculations
+"""
+
+
+def specific_fuel_consumption(fuel_mass_flow, power):
+    return fuel_mass_flow/power
+
+
 def main():
     plt.figure(figsize=(24, 12))
     file_paths = list(Path("gunnerus").glob("**/*.csv"))
@@ -139,7 +164,10 @@ def main():
     plot_individual(filtered_file_paths, "Engine coolant temperature")
 
     filtered_file_paths = [file_path for file_path in file_paths if is_engine_exhaust_temperature1(file_path)]
-    plot_individual(filtered_file_paths, "Engine coolant temperature")
+    plot_individual(filtered_file_paths, "Engine coolant temperature 1")
+
+    filtered_file_paths = [file_path for file_path in file_paths if is_engine_exhaust_temperature2(file_path)]
+    plot_individual(filtered_file_paths, "Engine coolant temperature 2")
 
 
 if __name__ == "__main__":
