827a9cf736360f818965f1c90ec22d22a4ca18d8
Pavel Skipenes <38912521+pavelskipenes@users.noreply.github.com>
2024-10-30 09:04:13 +0100

    simplify filter logic, fix plots not saved as svg

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def plot(x: list, y: list, title: str, label: str, clear: bool = True, save: boo
     plt.tight_layout()
     plt.plot()
     if save:
-        plt.savefig("plots/" + title.replace(".csv", ".svg"))
+        plt.savefig("plots/" + title + ".svg")
     if clear:
         plt.clf()
 
@@ -48,11 +48,11 @@ def is_engine_speed(file_path):
     return "engine_speed" in str(file_path)
 
 
-def is_load_feedback(file_path):
+def is_thruster_load_feedback(file_path):
     return "LoadFeedback" in str(file_path)
 
 
-def is_exhaust_temperature1(file_path):
+def is_engine_exhaust_temperature1(file_path):
     return "exhaust_temperature1" in str(file_path)
 
 
@@ -60,11 +60,11 @@ def is_exhaust_temperature2(file_path):
     return "exhaust_temperature2" in str(file_path)
 
 
-def is_coolant_temperature(file_path):
+def is_engine_coolant_temperature(file_path):
     return "coolant_temperature" in str(file_path)
 
 
-def is_boost_pressure(file_path):
+def is_engine_boost_pressure(file_path):
     return "boost_pressure" in str(file_path)
 
 
@@ -112,7 +112,7 @@ def plot_individual(filtered_file_paths, plot_title):
     for label in labels:
         (time, data) = time_series[label]
         plot(time, data, plot_title, label, False, False)
-    plt.savefig("plots/" + plot_title.replace(".csv", ".svg"))
+    plt.savefig("plots/" + plot_title + ".svg")
     plt.clf()
 
 
@@ -120,31 +120,25 @@ def main():
     plt.figure(figsize=(24, 12))
     file_paths = list(Path("gunnerus").glob("**/*.csv"))
 
-    filtered_file_paths = [file_path for file_path in file_paths if is_engine(file_path) and is_engine_load(file_path)]
+    filtered_file_paths = [file_path for file_path in file_paths if is_engine_load(file_path)]
     plot_inidividual_and_sum(filtered_file_paths, "Engine load")
 
-    filtered_file_paths = [file_path for file_path in file_paths if is_engine(
-        file_path) and is_engine_fuel_consumption(file_path)]
+    filtered_file_paths = [file_path for file_path in file_paths if is_engine_fuel_consumption(file_path)]
     plot_inidividual_and_sum(filtered_file_paths, "Fuel consumption")
 
-    filtered_file_paths = [file_path for file_path in file_paths if is_thruster(
-        file_path) and is_load_feedback(file_path)]
+    filtered_file_paths = [file_path for file_path in file_paths if is_thruster_load_feedback(file_path)]
     plot_individual(filtered_file_paths, "Load feedback")
 
-    filtered_file_paths = [file_path for file_path in file_paths if is_engine(
-        file_path) and is_engine_speed(file_path)]
+    filtered_file_paths = [file_path for file_path in file_paths if is_engine_speed(file_path)]
     plot_individual(filtered_file_paths, "Engine speed")
 
-    filtered_file_paths = [file_path for file_path in file_paths if is_engine(
-        file_path) and is_boost_pressure(file_path)]
+    filtered_file_paths = [file_path for file_path in file_paths if is_engine_boost_pressure(file_path)]
     plot_individual(filtered_file_paths, "Engine boost pressure")
 
-    filtered_file_paths = [file_path for file_path in file_paths if is_engine(
-        file_path) and is_coolant_temperature(file_path)]
+    filtered_file_paths = [file_path for file_path in file_paths if is_engine_coolant_temperature(file_path)]
     plot_individual(filtered_file_paths, "Engine coolant temperature")
 
-    filtered_file_paths = [file_path for file_path in file_paths if is_engine(
-        file_path) and is_exhaust_temperature1(file_path)]
+    filtered_file_paths = [file_path for file_path in file_paths if is_engine_exhaust_temperature1(file_path)]
     plot_individual(filtered_file_paths, "Engine coolant temperature")
 
 
